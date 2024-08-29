5e61a2bb18615162d47ae8ea5d27cdc896bc0322
qixin <379281867@qq.com>
2024-08-29 21:55:03 +0800

    bli新中地cesium-lesson13

--- blibliStudy/bli-xinzhongdi/blibli-xinzhongdi-cesium-vue3-project/src/App.vue
+++ blibliStudy/bli-xinzhongdi/blibli-xinzhongdi-cesium-vue3-project/src/App.vue
@@ -8,12 +8,13 @@ import Lesson8 from './components/Lesson08.vue';
 import Lesson9 from './components/Lesson09.vue';
 import Lesson10 from './components/Lesson10.vue';
 import Lesson11 from './components/Lesson11.vue';
+import Lesson13 from './components/Lesson13.vue';
 
 </script>
 
 <template>
   <div>
-    <Lesson11 ></Lesson11>
+    <Lesson13 ></Lesson13>
   </div>
   
 </template>
---
+++ blibliStudy/bli-xinzhongdi/blibli-xinzhongdi-cesium-vue3-project/src/components/Lesson13.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import * as Cesium from 'cesium'
+  import { onMounted } from 'vue'
+  
+  onMounted(()=>{
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NTYzNjEwZC1kODRkLTQ3YWMtOWM0Ny1mYTAxYTQ0NDI1MTIiLCJpZCI6MjM0Mjc0LCJpYXQiOjE3MjM0NzIzNTB9.erRR0UJ-3jMrzQSa6udAIyxjLb142DdsN42fihQYAu4';
+    // 所有api的开始
+    
+    const viewer = new Cesium.Viewer('cesiumContainer', {})
+
+    // 盒子
+    const box = viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(119, 30, 3000),
+      box: {
+        dimensions: new Cesium.Cartesian3(2000, 1000, 3000), // 长宽高
+        material: Cesium.Color.RED,
+      }
+    });
+
+    viewer.zoomTo(box);
+
+  })
+  
+  </script>
+  
+  <template>
+    <div>13 entity实体（盒子、椭圆、立体）</div>
+    <div id="cesiumContainer">
+      
+    </div>
+    
+  </template>
+  
+  <style scoped>
+  #cesiumContainer {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+  
+  </style>
+  <style>
+  .cesium-viewer-bottom {
+    display: none;
+  }
+  </style>
+  
